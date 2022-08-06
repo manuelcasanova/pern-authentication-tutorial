cad60b70d39955ba696af808d29c900011a9486b
Manuel Casanova <manucasanova@hotmail.com>
2022-08-06 08:24:29 -0700

    Create schema to build routes

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -3,4 +3,25 @@
 const router = require("express").Router()
 
 
+//registering
+
+router.post("/", async(req, res) => {
+  try {
+
+    //1. Destructure the req.body (name, email, password)
+
+    //2. Check if user exist (if exists, then throw error)
+
+    //3. Bcrypt the password
+
+    //4. Enter the user in the db
+
+    //5. generate our jwt token
+    
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+})
+
 module.exports = router;
