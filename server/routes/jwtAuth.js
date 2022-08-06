b87a68013424d6374e97befc6ad7d1dfa9617975
Manuel Casanova <manucasanova@hotmail.com>
2022-08-06 08:35:13 -0700

    Start building the route for registration. Destructure the req body Write query to check if user exists

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,23 +1,36 @@
 //Router allows us to break down the routes and combine them together within the index,js
 
 const router = require("express").Router()
+const pool = require("../db")
 
 
 //registering
 
-router.post("/", async(req, res) => {
+router.post("/register", async(req, res) => {
   try {
 
     //1. Destructure the req.body (name, email, password)
 
+    const {name, email, password} = req.body
+
     //2. Check if user exist (if exists, then throw error)
+
+  const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+    email
+  ]);
+
+  if (user.rows.length !== 0) {
+    return res.status(401).send("Email already exists");
+  } 
+  
+  res.json(user.rows)
 
     //3. Bcrypt the password
 
     //4. Enter the user in the db
 
     //5. generate our jwt token
-    
+
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
