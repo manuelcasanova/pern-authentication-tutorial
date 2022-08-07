591ba3eb75a5d78465b70b352380fc029fbdf908
Manuel Casanova <manucasanova@hotmail.com>
2022-08-06 18:41:59 -0700

    Set protected routes. It does not follow the tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, createContext } from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route, Redirect } from "react-router-dom"
-
+import { BrowserRouter as Router, Routes, Route, Redirect, UNSAFE_RouteContext } from "react-router-dom"
+import ProtectedRoutes from './components/ProtectedRoutes';
 
 //Components
 
@@ -10,23 +10,29 @@ import Dashboard from './components/Dashboard';
 import Register from './components/Register';
 import Login from './components/Login';
 
+export const UserContext = createContext()
 
 function App() {
 
-const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <Fragment>
-      <Router>
-        <div className="container">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </div>
-      </Router>
-    </Fragment>
+    <UserContext.Provider value={{isAuthenticated}}>
+      <Fragment>
+        <Router>
+          <div className="container">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route element={<ProtectedRoutes />}>
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Route>
+            </Routes>
+          </div>
+        </Router>
+      </Fragment>
+    </UserContext.Provider>
+
   );
 }
 
