ff4857e48282c69145d5510a1c2015b6bf82c48d
Manuel Casanova <manucasanova@hotmail.com>
2022-08-06 17:27:29 -0700

    Write query. Finish route

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,8 +2,15 @@ const router = require("express").Router()
 const pool = require("../db")
 const authorization = require("../middleware/authorization")
 
-router.get("/", authorization, async(req, res) => {
+router.get("/", authorization, async (req, res) => {
   try {
+    //Pay attention to the authirization.js. If successfull, we have access to the user
+    //req.user has the payload
+    //res.json(req.user);
+
+    const user = await pool.query("SELECT user_name FROM users WHERE user_id = $1", [req.user]);
+
+    res.json(user.rows[0]);
 
   } catch (err) {
     console.error(err.message);
