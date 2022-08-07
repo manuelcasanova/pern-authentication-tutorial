caf07c417382ee163bd0304a528a39f2257a1476
Manuel Casanova <manucasanova@hotmail.com>
2022-08-06 17:47:07 -0700

    Import BrowserRouter and others

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import { Fragment } from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom"
 
 function App() {
   return (
     <Fragment>
-      
+
     </Fragment>
   );
 }
