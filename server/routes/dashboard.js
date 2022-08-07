0fc9140bc232b3a6bfae94c424de35b4773ba68a
Manuel Casanova <manucasanova@hotmail.com>
2022-08-06 17:21:17 -0700

    Start writing try catch for route

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,3 +1,14 @@
 const router = require("express").Router()
+const pool = require("../db")
+const authorization = require("../middleware/authorization")
+
+router.get("/", authorization, async(req, res) => {
+  try {
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server Error");
+  }
+});
 
 module.exports = router
